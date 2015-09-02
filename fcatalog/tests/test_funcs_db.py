81f599d9380566929a05833762e40692216f67f7
xorpd <xorpd@xorpd.net>
2015-09-02 15:01:38 +0300

    Open connection only once fixes + test_add_function with counting.

--- fcatalog/tests/test_funcs_db.py
+++ fcatalog/tests/test_funcs_db.py
@@ -1,7 +1,4 @@
 import pytest
-import os
-import shutil
-import tempfile
 import sqlite3
 
 from funcs_db import FuncsDB
@@ -12,47 +9,54 @@ class DebugFuncsDB(FuncsDB):
         """
         Count the amount of lines inside the funcs table.
         """
-        conn = sqlite3.connect(self._db_path)
-        c = conn.cursor()
+        c = self._conn.cursor()
 
         cmd_count = "SELECT COUNT(*) from funcs"
         c.execute(cmd_count)
         res = c.fetchone()[0]
-        conn.close()
 
         return res
 
 
 @pytest.fixture(scope='module')
-def fdb(request):
+def fdb_mem(request):
     """
     Create a FuncsDB instance (A new database on disk).
     """
-    # Create a temporary directory:
-    p = tempfile.mkdtemp()
-    db_path = os.path.join(p,'my_db.db')
-    fdb = DebugFuncsDB(db_path,16)
+    # Build database in memory. Should be quicker.
+    fdb = DebugFuncsDB(':memory:',16)
 
     def fin():
-        """Finalization of the fixture"""
-        # Remove the database:
-        shutil.rmtree(db_path)
+        """Finalizer for fdb"""
+        # Make sure to close fdb.
+        fdb.close()
+
+    request.addfinalizer(fin)
     return fdb
 
 
-def test_funcs_db_basic(fdb):
+def test_funcs_db_basic(fdb_mem):
     """
     Create a new database using FuncsDB.
     """
     pass
 
 
-def test_add_function(fdb):
+def test_add_function(fdb_mem):
     """
     Try to add a 'reversed' new function into the database.
     """
-    assert fdb.count() == 0
-    fdb.add_function('func_name',b'func_data','func_comment')
-    assert fdb.count() == 1
+    # No functions at the beginning:
+    assert fdb_mem.count() == 0
+    # Add one function:
+    fdb_mem.add_function('func_name1',b'func_data1','func_comment1')
+    assert fdb_mem.count() == 1
+    # Add another function:
+    fdb_mem.add_function('func_name2',b'func_data2','func_comment2')
+    assert fdb_mem.count() == 2
+    # Add a function with func_data that already exists. Should replace the
+    # earlier func_data2 record (Instead of adding a new row).
+    fdb_mem.add_function('func_name3',b'func_data2','func_comment3')
+    assert fdb_mem.count() == 2
 
 
