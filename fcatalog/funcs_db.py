c38fe24a2879f95086606bea9fd77e033e7dafdf
xorpd <xorpd@xorpd.net>
2015-09-02 15:01:06 +0300

    Opening connection to DB only once.

--- fcatalog/funcs_db.py
+++ fcatalog/funcs_db.py
@@ -3,6 +3,9 @@ import os
 
 from catalog1 import sign,strong_hash
 
+class FuncsDBError(Exception):
+    pass
+
 
 class FuncsDB:
     def __init__(self,db_path,num_hashes):
@@ -10,9 +13,35 @@ class FuncsDB:
         self._db_path = db_path
         self._num_hashes = num_hashes
 
-        # If the database file does not exist, we create an empty database:
-        if not os.path.isfile(db_path):
+        # Check if the db has existed before:
+        db_existed = False
+        if os.path.isfile(db_path):
+            db_existed = True
+
+        # Open a connection to the database.
+        self._conn = sqlite3.connect(self._db_path)
+        self._is_open = True
+        
+        # If the database file did not exist, we create an empty database:
+        if not db_existed:
             self._build_empty_db()
+        
+
+    def _check_is_open(self):
+        """
+        Make sure that this FuncsDB instance is open.
+        """
+        if not self._is_open:
+            raise FuncsDBError('FuncsDB instance is closed')
+
+
+    def close(self):
+        """
+        Close the connection to the database.
+        """
+        self._check_is_open()
+        self._conn.close()
+
 
 
     def _build_empty_db(self):
@@ -20,9 +49,8 @@ class FuncsDB:
         Build an initial empty database.
         Create tables and indices.
         """
-
-        conn = sqlite3.connect(self._db_path)
-        c = conn.cursor()
+        self._check_is_open()
+        c = self._conn.cursor()
 
         cmd_tbl = \
             """CREATE TABLE funcs(
@@ -47,19 +75,18 @@ class FuncsDB:
                     'funcs(' + cname + ');'
             c.execute(cmd_index)
 
-        conn.commit()
-        conn.close()
+        self._conn.commit()
 
 
     def add_function(self,func_name,func_data,func_comment):
         """
         Add a (Reversed) function to the database.
         """
+        self._check_is_open()
         s = sign(func_data,self._num_hashes)
         func_hash = strong_hash(func_data)
 
-        conn = sqlite3.connect(self._db_path)
-        c = conn.cursor()
+        c = self._conn.cursor()
 
         cmd_insert = \
                 """INSERT OR REPLACE into funcs 
@@ -70,14 +97,18 @@ class FuncsDB:
 
         cmd_insert += ') values (?,?,?' + (',?' * self._num_hashes) + ');'
 
-        conn.execute(cmd_insert,[\
+        self._conn.execute(cmd_insert,[\
                 sqlite3.Binary(func_hash),func_name,func_comment] + s)
 
-
-        conn.commit()
-        conn.close()
+        self._conn.commit()
 
 
     def get_similars(self,func_data,num_similars):
+        """
+        Get a list of at most num_similars similar functions to a given
+        function. The list will be ordered by similarity. The first element is
+        the most similar one.
+        """
+        self._check_is_open()
         pass
 
