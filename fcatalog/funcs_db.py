c81110260d297be69b82e9f084a85d0ebc607948
xorpd <xorpd@xorpd.net>
2015-09-02 14:36:44 +0300

    Added add_function.

--- fcatalog/funcs_db.py
+++ fcatalog/funcs_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-from catalog1 import sign
+from catalog1 import sign,strong_hash
 
 
 class FuncsDB:
@@ -28,13 +28,13 @@ class FuncsDB:
             """CREATE TABLE funcs(
                 hash BLOB PRIMARY KEY,
                 func_name TEXT NOT NULL,
-                head_comment TEXT NOT NULL"""
+                func_comment TEXT NOT NULL"""
 
         for i in range(self._num_hashes):
             cmd_tbl += ',\n'
             cmd_tbl += 'c' + str(i+1) + ' INTEGER NOT NULL'
 
-        cmd_tbl += ')'
+        cmd_tbl += ');'
 
         # Create the funcs table:
         print(cmd_tbl)
@@ -44,17 +44,40 @@ class FuncsDB:
         for i in range(self._num_hashes):
             cname = 'c' + str(i+1)
             cmd_index = 'CREATE INDEX idx_' + cname + ' ON ' + \
-                    'funcs(' + cname + ')'
+                    'funcs(' + cname + ');'
             c.execute(cmd_index)
 
         conn.commit()
         conn.close()
 
 
-    def add_function(self,func_name,func_data,head_comment):
-        pass
+    def add_function(self,func_name,func_data,func_comment):
+        """
+        Add a (Reversed) function to the database.
+        """
+        s = sign(func_data,self._num_hashes)
+        func_hash = strong_hash(func_data)
+
+        conn = sqlite3.connect(self._db_path)
+        c = conn.cursor()
+
+        cmd_insert = \
+                """INSERT OR REPLACE into funcs 
+                    (hash,func_name,func_comment"""
+
+        for i in range(self._num_hashes):
+            cmd_insert += ',c' + str(i+1) + ' '
+
+        cmd_insert += ') values (?,?,?' + (',?' * self._num_hashes) + ');'
+
+        conn.execute(cmd_insert,[\
+                sqlite3.Binary(func_hash),func_name,func_comment] + s)
+
+
+        conn.commit()
+        conn.close()
+
 
     def get_similars(self,func_data,num_similars):
         pass
-
 
