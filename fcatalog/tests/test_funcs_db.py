df84e5e5ce6d75a1f2800837106fdc6b96a7306b
xorpd <xorpd@xorpd.net>
2015-09-03 20:04:43 +0300

    Added some tests for get_similars. Tests pass.

--- fcatalog/tests/test_funcs_db.py
+++ fcatalog/tests/test_funcs_db.py
@@ -1,3 +1,4 @@
+import unittest
 import pytest
 import sqlite3
 import random
@@ -99,6 +100,10 @@ def test_get_similars_basic(fdb_mem):
     assert len(res) == 1
 
 
+########################################
+########################################
+
+
 def test_random_seed(fdb_mem):
     """
     We expect consistency when using python's random with seed.
@@ -123,43 +128,126 @@ def rand_ascii_lowercase(n):
             for _ in range(n))
 
 
-
-def test_get_similars(fdb_mem):
+def num_matches(s1,s2):
     """
-    Test the operation of get_similars with many functions inside the DB.
+    Calculate the amount of entries where s1[i] == s2[i]
     """
-    # Pick an initial seed, for determinisim.
-    random.seed(a='A seed for determinism of this test.')
+    assert len(s1) == len(s2)
+    return sum(1 for i in range(len(s1)) if s1[i] == s2[i])
 
-    num_funcs = 0x30
-    func_size = 0xf0
-    func_name_len = 0x20
+def test_num_matches():
+    """
+    Make sure that num_matches works correctly.
+    """
+    s1 = [1,2,3,4]
+    s2 = [1,2,8,4]
+    s3 = [0,9,4,3]
+    assert num_matches(s1,s2) == 3
+    assert num_matches(s1,s1) == 4
+    assert num_matches(s1,s3) == 0
 
-    # Index of the special function. We will later search a small variation of
-    # the function inside the database, and expect to find this function.
-    special_func_idx = random.randrange(num_funcs)
-    special_func = None
+def change_random_byte(bs):
+    """
+    Change a random byte to another random byte in a stream of bytes.
+    Return the new bytes stream.
+    """
+    # Nothing to do if bs is empty:
+    if len(bs) == 0:
+        return bs
 
-
-    for i in range(num_funcs):
-        # Generate function data:
-        func_data = rand_bytes(func_size)
-        # Generate random name and comment:
-        func_name = rand_ascii_lowercase(func_name_len)
-        func_comment = rand_ascii_lowercase(func_name_len)
-
-        s = sign(func_data,16)
-        # Add the function:
-        # fdb_mem.add_function(func_name,func_data,func_comment)
-
-        # If this is the special function, we keep it for later.
-        if i == special_func_idx:
-            special_func_data = bytes(func_data)
-            special_func_name = func_name
-            special_func_comment = func_comment
+    # Pick a random index:
+    idx = random.randrange(len(bs))
+    return bs[:idx] + rand_bytes(1) + bs[idx+1:]
 
 
+class TestGetSimilars(unittest.TestCase):
+    def setUp(self):
+        """Tests setup"""
+        # Build database in memory. Should be quicker.
+        self.fdb_mem = DebugFuncsDB(':memory:',NUM_HASHES)
+
+        # Pick an initial seed, for determinisim.
+        random.seed(a='A seed for determinism of this test.')
+
+        self.num_funcs = 0x400
+        self.func_size = 0xf0
+        self.func_name_len = 0x20
+
+        # Index of the special function. We will later search a small variation
+        # of the function inside the database, and expect to find this
+        # function.
+        special_func_idx = random.randrange(self.num_funcs)
+        special_func = None
+
+        # Insert functions to db:
+        for i in range(self.num_funcs):
+            # Generate function data:
+            func_data = rand_bytes(self.func_size)
+            # Generate random name and comment:
+            func_name = rand_ascii_lowercase(self.func_name_len)
+            func_comment = rand_ascii_lowercase(self.func_name_len)
+
+            # s = sign(func_data,16)
+            # Add the function:
+            self.fdb_mem.add_function(func_name,func_data,func_comment)
+
+            # If this is the special function, we keep it for later.
+            if i == special_func_idx:
+                self.special_func_data = bytes(func_data)
+                self.special_func_name = func_name
+                self.special_func_comment = func_comment
 
 
+    def test_match_random_func(self):
+        """
+        Try to find similarity for a random function. We expect that now
+        results will show up at all.
+        """
+        # Generate a random function:
+        func_data = rand_bytes(self.func_size)
+        sims = self.fdb_mem.get_similars(func_data,1)
+        # We assume that no function is similar to func_data (Even in one entry),
+        # so we expect to get no results. Getting a result is possible, but very
+        # surprising.
+        assert len(sims) == 0
+
+    def test_match_special_func(self):
+        """
+        Try to find similarity for the special function. We expect to find it.
+        """
+        sims = self.fdb_mem.get_similars(self.special_func_data,5)
+        # We assume that only the special function will return:
+        assert len(sims) == 1
+
+        assert sims[0].func_hash == strong_hash(self.special_func_data)
+        assert sims[0].func_name == self.special_func_name
+        assert sims[0].func_comment == self.special_func_comment
+        assert sims[0].func_sig == sign(self.special_func_data,NUM_HASHES)
+
+
+    def test_match_like_special_func(self):
+        """
+        Try to find similarity for a function that is just a bit different from
+        the special function. We expect to find the special function.
+        """
+        func_data = self.special_func_data
+        # Change some bytes randomly:
+        for i in range(5):
+            func_data = change_random_byte(func_data)
+
+        sims = self.fdb_mem.get_similars(func_data,5)
+        # We assume that only the special function will return:
+        assert len(sims) == 1
+
+        assert sims[0].func_hash == strong_hash(self.special_func_data)
+        assert sims[0].func_name == self.special_func_name
+        assert sims[0].func_comment == self.special_func_comment
+        assert sims[0].func_sig == sign(self.special_func_data,NUM_HASHES)
+
+
+    def tearDown(self):
+        """Tests teardown"""
+        # Close the database:
+        self.fdb_mem.close()
 
 
