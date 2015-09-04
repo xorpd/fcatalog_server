a086e469746a026f47caa07b2bd1ad944d4aa172
xorpd <xorpd@xorpd.net>
2015-09-04 13:49:59 +0300

    Changes to arguments of the profiler.

--- fcatalog/psign.py
+++ fcatalog/psign.py
@@ -1,7 +1,12 @@
 # Profiling code for the sign function.
 
+import os
 import random
 from catalog1 import sign,strong_hash
+from funcs_db import FuncsDB
+
+DB_PATH = '/tmp/my_test_db.db'
+NUM_HASHES = 16
 
 def rand_bytes(n):
     """
@@ -13,16 +18,30 @@ def go():
     # Pick an initial seed, for determinisim.
     random.seed(a='A seed for determinism of this test.')
 
+    # Delete a current db if existent:
+    try:
+        os.remove(DB_PATH)
+    except FileNotFoundError:
+        pass
+
+    # Build database in memory. Should be quicker.
+    fdb = FuncsDB(DB_PATH,NUM_HASHES)
+
     num_funcs = 64000
     func_size = 0x200
     func_name_len = 0x20
 
 
+    # Add some functions:
     for i in range(num_funcs):
         # Generate function data:
         func_data = rand_bytes(func_size)
         # Calculate signature:
-        s = sign(func_data,16)
+        # s = sign(func_data,16)
+        # Add the function to the DB:
+        fdb.add_function('name',func_data,'comment')
+
+    fdb.close()
 
     print('done!')
 
