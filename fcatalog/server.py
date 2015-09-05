f2df03a58bf6379bf471b8609f58524c1053f2b0
xorpd <xorpd@xorpd.net>
2015-09-05 16:56:40 +0300

    Initial definition of messages for the server.

--- fcatalog/server.py
+++ fcatalog/server.py
@@ -1,17 +1,64 @@
-class AddFunction(MsgDef):
-    pass
-
-class GetSimilars(MsgDef):
-    pass
-
-class ResultSimilars(MsgDef):
-    pass
 
 class ChooseDB(MsgDef):
-    pass
+    afields = ['db_name']
+    def serialize(self,msg_inst) -> bytes:
+        """
+        Serialize a msg_inst into bytes.
+        """
+        raise NotImplementedError()
+
+    def deserialize(self,msg_data:bytes):
+        """
+        Deserialize data bytes into a msg_inst.
+        """
+        raise NotImplementedError()
+
+
+class AddFunction(MsgDef):
+    afields = ['func_name','func_comment','func_data']
+    def serialize(self,msg_inst) -> bytes:
+        """
+        Serialize a msg_inst into bytes.
+        """
+        raise NotImplementedError()
+
+    def deserialize(self,msg_data:bytes):
+        """
+        Deserialize data bytes into a msg_inst.
+        """
+        raise NotImplementedError()
+
+class RequestSimilars(MsgDef):
+    afields = ['func_data','num_similars']
+    def serialize(self,msg_inst) -> bytes:
+        """
+        Serialize a msg_inst into bytes.
+        """
+        raise NotImplementedError()
+
+    def deserialize(self,msg_data:bytes):
+        """
+        Deserialize data bytes into a msg_inst.
+        """
+        raise NotImplementedError()
+
+class ResponseSimilars(MsgDef):
+    afields = ['similars']
+    def serialize(self,msg_inst) -> bytes:
+        """
+        Serialize a msg_inst into bytes.
+        """
+        raise NotImplementedError()
+
+    def deserialize(self,msg_data:bytes):
+        """
+        Deserialize data bytes into a msg_inst.
+        """
+        raise NotImplementedError()
 
 
 proto_def = {\
-        0:AddFunction(),\
-        1:GetSimilars(),\
-        2:ChooseDB()}
+        0:ChooseDB,\
+        1:AddFunction,\
+        2:RequestSimilars,\
+        3:ResponseSimilars}
